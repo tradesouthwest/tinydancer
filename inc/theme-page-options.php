383faf531c3557ed80e6f96d988215fd31975482
tradesouthwest <tradesouthwest@gmail.com>
2023-06-23 16:13:16 -0700

    options page

--- inc/theme-page-options.php
+++ inc/theme-page-options.php
@@ -35,11 +35,12 @@ function tinydancer_sanitize_text( $input ) {
 function tinydancer_theme_customizer_css() {
     if ( get_theme_mods() ) : 
         $background_color = get_background_color();
-        echo '<style type="text/css">';
+        echo '<style id="tinydancer-theme-mods">';
         if ( get_theme_mod( 'tinydancer-font-color' ) ) :
             $cpaddg = get_theme_mod( 'tinydancer_padding_content' );
             $pcolor = get_theme_mod( 'tinydancer-font-color' );
-            echo 'body{ background-color: #' . esc_attr( $background_color ) . ';}.section-content{ padding: ' . esc_attr( $cpaddg ) . 'px; }.section-sidebar{padding-top: ' . esc_attr( $cpaddg ) . 'px;}p, .entry-content, .excerpt-content, li, h2, h3, h4 {color: ' . esc_attr( $pcolor ) . ';}';
+            $dloop  = get_theme_mod( 'tinydancer_blog_layout' ); 
+            echo 'body{ background-color: #' . esc_attr( $background_color ) . ';}.section-content{ padding: ' . esc_attr( $cpaddg ) . 'px; }.section-sidebar{padding-top: ' . esc_attr( $cpaddg ) . 'px;}.dancer-loop{flex-direction: ' . esc_attr( $dloop ) . ';}p, .entry-content, .excerpt-content, li, h2, h3, h4 {color: ' . esc_attr( $pcolor ) . ';}';
         endif;
         echo '</style>';
     endif;
