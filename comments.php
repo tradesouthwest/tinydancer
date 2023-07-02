4a7b38d3ab1fd19cd1c42d25a34742b6cd8ba205
tradesouthwest <tradesouthwest@gmail.com>
2023-07-02 11:44:54 -0700

    escaping

--- comments.php
+++ comments.php
@@ -46,7 +46,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 		?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'classicsixteen' ); ?></p>
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'tinydancer' ); ?></p>
 	<?php endif; ?>
 
 	<?php
