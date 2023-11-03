69a8addff503b9a27f642243042ca2725727a8ca
Larry <tradesouthwest@outlook.com>
2023-11-03 11:59:10 -0700

    Update theme-page-options.php

--- inc/theme-page-options.php
+++ inc/theme-page-options.php
@@ -4,7 +4,8 @@
  * @since 1.0
  * @package tinydancer
  */
-add_action( 'wp_head', 'tinydancer_theme_customizer_css');
+add_action( 'tinydancer_theme_customizer', 'tinydancer_theme_customizer_css' );
+		
 /**
  * Text sanitizer for numeric values
  * @since 1.0
@@ -33,18 +34,29 @@ function tinydancer_sanitize_text( $input ) {
  * 
  */
 function tinydancer_theme_customizer_css() {
+    $styles = '';
+    $styles .= '<style id="tinydancer-theme-mods">';
+
     if ( get_theme_mods() ) : 
         $background_color = get_background_color();
-        echo '<style id="tinydancer-theme-mods">';
-        if ( get_theme_mod( 'tinydancer-font-color' ) ) :
+        
+        
             $cpaddg = get_theme_mod( 'tinydancer_padding_content' );
             $pcolor = get_theme_mod( 'tinydancer-font-color' );
             $dloop  = get_theme_mod( 'tinydancer_blog_layout' ); 
             $calign = get_theme_mod( 'tinydancer_content_align' ); 
-            $cposit = (empty(get_theme_mod( 'tinydancer_hero_callposition' )))? '12' : get_theme_mod( 'tinydancer_hero_callposition' ); 
-            $hheight = (empty(get_theme_mod( 'tinydancer_hero_bkgheight' )))? '520' : get_theme_mod( 'tinydancer_hero_bkgheight' ); 
-            echo 'body{ background-color: #' . esc_attr( $background_color ) . ';}.section-content{ padding: ' . esc_attr( $cpaddg ) . 'px;text-align:' . esc_attr( $calign ) .'; }.section-sidebar{padding-top: ' . esc_attr( $cpaddg ) . 'px;}.hero-inner-content{height: calc( '. absint($hheight).'px - 10vh ); }.page-header{padding: calc(.5 * ' . esc_attr( $cpaddg ) . 'px);padding-bottom: 0;}.dancer-loop{flex-direction: ' . esc_attr( $dloop ) . ';}p, .entry-content, .excerpt-content, li, h2, h3, h4 {color: ' . esc_attr( $pcolor ) . ';}.call-to-action{top: '. esc_attr( $cposit ) . 'em;}';
-        endif;
-        echo '</style>';
+            $cposit = (empty(get_theme_mod( 'tinydancer_hero_callposition' ) ) )
+                    ? '12' : get_theme_mod( 'tinydancer_hero_callposition' ); 
+            $hheight = (empty(get_theme_mod( 'tinydancer_hero_bkgheight' ) ) ) 
+                    ? '520' : get_theme_mod( 'tinydancer_hero_bkgheight' ); 
+        
+
+    $styles .= 'body{ background-color: #' . esc_attr( $background_color ) . ';}.section-content{ padding: ' . esc_attr( $cpaddg ) . 'px;text-align:' . esc_attr( $calign ) .'; }.section-sidebar{padding-top: ' . esc_attr( $cpaddg ) . 'px;}.hero-inner-content{height: calc( '. absint($hheight).'px - 10vh ); }.page-header{padding: calc(.5 * ' . esc_attr( $cpaddg ) . 'px);padding-bottom: 0;}.dancer-loop{flex-direction: ' . esc_attr( $dloop ) . ';}p, .entry-content, .excerpt-content, li, h2, h3, h4 {color: ' . esc_attr( $pcolor ) . ';}
+            .call-to-action{top: '. esc_attr( $cposit ) . 'em;}';
+    
+        
+	
     endif;
+    $styles .= '</style>';
+    echo $styles;
 }
